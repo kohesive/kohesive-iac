82be0b8360924482f441b6832cafa31954bafa28
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-02-24 14:17:58 +0700

    makeListProxy

--- model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/proxy/ProxyUtils.kt
+++ model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/proxy/ProxyUtils.kt
@@ -11,10 +11,23 @@ object ProxyUtils {
 
 fun createReference(targetId: String, property: String) = "{{kohesive:ref:$targetId:$property}}"
 
+inline fun <S, reified T : Any> makeListProxy(
+    context: IacContext,
+    baseId: String,
+    requestObjects: List<S>,
+    copyFromReq: Map<KFunction1<S, Any>, KFunction1<T, Any>>,
+    includeReferences: List<KFunction1<T, Any>>  = ProxyUtils.INCLUDE_ALL_PROPS,
+    disallowReferences: List<KFunction1<T, Any>> = copyFromReq.values.toList()
+): List<T> {
+    return requestObjects.mapIndexed { idx, obj ->
+        makeProxy(context, "$baseId[$idx]", obj, copyFromReq, includeReferences, disallowReferences)
+    }
+}
+
 inline fun <S, reified T : Any> makeProxy(
     context: IacContext,
     id: String,
-    sourceObj: S,
+    requestObject: S,
     copyFromReq: Map<KFunction1<S, Any>, KFunction1<T, Any>>,
     includeReferences: List<KFunction1<T, Any>>  = ProxyUtils.INCLUDE_ALL_PROPS,
     disallowReferences: List<KFunction1<T, Any>> = copyFromReq.values.toList()
@@ -23,7 +36,7 @@ inline fun <S, reified T : Any> makeProxy(
         val delegate = T::class.java.newInstance()
         (Enhancer.create(T::class.java, MethodInterceptor { obj, method, args, proxy ->
             copyFromReq.keys.firstOrNull { it.name == method.name }?.let { sourceFunction ->
-                return@MethodInterceptor sourceFunction.invoke(sourceObj)
+                return@MethodInterceptor sourceFunction.invoke(requestObject)
             }
             if (method.name.startsWith("get") && method.returnType == String::class.java) {
                 if (disallowReferences.any { it.name == method.name }) {
