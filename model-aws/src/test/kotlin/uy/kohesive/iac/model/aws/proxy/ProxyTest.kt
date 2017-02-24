c3ef841deca09c33cc70ef189e19f304ce0727bf
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-02-24 17:03:19 +0700

    ProxyTest unit test

---
+++ model-aws/src/test/kotlin/uy/kohesive/iac/model/aws/proxy/ProxyTest.kt
@@ -0,0 +1,50 @@
+package uy.kohesive.iac.model.aws.proxy
+
+import com.amazonaws.services.ec2.model.Instance
+import com.amazonaws.services.ec2.model.Reservation
+import com.amazonaws.services.ec2.model.RunInstancesRequest
+import com.amazonaws.services.ec2.model.RunInstancesResult
+import junit.framework.TestCase
+import kotlin.test.assertFails
+
+class ProxyTest : TestCase() {
+
+    fun testSome() {
+        val context = IacContext(
+            environment = "test",
+            planId      = "somePlan"
+        )
+
+        val request = RunInstancesRequest()
+            .withImageId("myImageId")
+            .withMaxCount(12)
+
+        val result  = RunInstancesResult().withReservation(
+            Reservation().withInstances(
+                makeProxy<RunInstancesRequest, Instance>(context, "MyInstance", request, mapOf(
+                        RunInstancesRequest::getImageId to Instance::getImageId
+                    ),
+                    disallowReferences = listOf(Instance::getArchitecture)
+                )
+            )
+        )
+
+        assertNotNull(result.reservation.instances.firstOrNull())
+        val instance = result.reservation.instances[0]
+
+        // Id
+        assertEquals("MyInstance", context.getId(instance))
+
+        // Copied property
+        assertEquals("myImageId", instance.imageId)
+
+        // New ref-property
+        assertEquals("{{kohesive:ref:MyInstance:PublicDnsName}}", instance.publicDnsName)
+
+        // Disallowed property
+        assertFails {
+            println(instance.architecture)
+        }
+    }
+
+}
