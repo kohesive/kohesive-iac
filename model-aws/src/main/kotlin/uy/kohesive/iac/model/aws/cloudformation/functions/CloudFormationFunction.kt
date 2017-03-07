69bd998f8879c89b0bf1965409a8eec04d8bb0b3
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-03-07 14:12:49 +0700

    CloudFormation functions definitions/serialization

---
+++ model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudformation/functions/CloudFormationFunction.kt
@@ -0,0 +1,57 @@
+package uy.kohesive.iac.model.aws.cloudformation.functions
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
+interface CloudFormationFunction {
+    val name: String
+    val args: Any?
+}
+
+@JsonSerialize(using = CloudFormationFunctionSerializer::class)
+class CFBase64Function(value: Any?) : CloudFormationFunction {
+    override val name = "Fn::Base64"
+    override val args = value
+}
+
+@JsonSerialize(using = CloudFormationFunctionSerializer::class)
+class CFJoinFunction(args: List<Any>, delimiter: Any = "") : CloudFormationFunction {
+    override val name = "Fn::Base64"
+    override val args = listOf(delimiter) + listOf(args)
+}
+
+@JsonSerialize(using = CloudFormationFunctionSerializer::class)
+class CFFindInMapFunction(mapName: String, topLevelKey: Any, secondLevelKey: Any) : CloudFormationFunction {
+    override val name = "Fn::FindInMap"
+    override val args = listOf(mapName, topLevelKey, secondLevelKey)
+}
+
+@JsonSerialize(using = CloudFormationFunctionSerializer::class)
+class CFRefFunction(target: Any) : CloudFormationFunction {
+    override val name = "Ref"
+    override val args = target
+}
+
+@JsonSerialize(using = CloudFormationFunctionSerializer::class)
+class CFGetAvailabilityZonesFunction(arg: String = "") : CloudFormationFunction {
+    override val name = "Fn::GetAZs"
+    override val args = arg
+}
+
+@JsonSerialize(using = CloudFormationFunctionSerializer::class)
+class CFGetAttributeFunction(logicalNameOfResource: Any, attributeName: Any) : CloudFormationFunction {
+    override val name = "Fn::GetAtt"
+    override val args = listOf(logicalNameOfResource, attributeName)
+}
+
+class CloudFormationFunctionSerializer : JsonSerializer<CloudFormationFunction>() {
+
+    override fun serialize(value: CloudFormationFunction, gen: JsonGenerator, serializers: SerializerProvider) {
+        gen.writeStartObject()
+        gen.writeObjectField(value.name, value.args)
+        gen.writeEndObject()
+    }
+
+}
