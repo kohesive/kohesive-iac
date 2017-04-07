d7910f87b4aaad180a80d7d1ab92a6d9e612dd19
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-04-07 18:35:02 +0700

    Smarter getOrCreateShape (previous one was pretty stupid)

--- model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/codegen/s3/ModelFromAPIGenerator.kt
+++ model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/codegen/s3/ModelFromAPIGenerator.kt
@@ -11,6 +11,8 @@ import org.reflections.scanners.TypeElementsScanner
 import uy.klutter.core.common.mustNotStartWith
 import uy.kohesive.iac.model.aws.utils.firstLetterToUpperCase
 import uy.kohesive.iac.model.aws.utils.namespace
+import uy.kohesive.iac.model.aws.utils.simpleName
+import java.util.*
 
 class ModelFromAPIGenerator(
     val serviceInterface: Class<*>,
@@ -25,9 +27,11 @@ class ModelFromAPIGenerator(
     private val packageName: String = serviceInterface.canonicalName.namespace()
     private val modelPackage = packageName + ".model"
 
-    private val typeElementsScanner = TypeElementsScanner().apply {
+    private val modelClassFqNamesBySimpleName = TypeElementsScanner().apply {
         Reflections(modelPackage, this)
-    }
+    }.store.keySet().associateBy(String::simpleName) + mapOf(
+        "HttpMethod" to "com.amazonaws.HttpMethod"
+    )
 
     private val sources = SourceCache(serviceSourcesDir)
 
@@ -39,55 +43,71 @@ class ModelFromAPIGenerator(
         null
     )
 
-    private val shapes = HashMap<String, Shape>().apply {
-        listOf(
-            java.lang.String::class.java,
-            java.lang.Integer::class.java,
-            java.lang.Long::class.java,
-            java.lang.Boolean::class.java,
-            java.lang.Void::class.java
-        ).forEach { autoboxType ->
-            put(autoboxType.simpleName, Shape().apply {
-                type = autoboxType.simpleName.toLowerCase()
-            })
-        }
-    }
+    private val classFqNameToShape = HashMap<String, Pair<String, Shape>>()
 
-    private fun createShape(shapeName: String, clazz: Class<*>) = Shape().apply {
-        val membersByGetters = clazz.declaredMethods.filter { method ->
-            method.name.startsWith("get") || method.name.startsWith("is")
-        }.map { getter ->
-            getter.name.mustNotStartWith("get").mustNotStartWith("is") to getter.returnType
-        }.toMap()
+    private fun getModelClassBySimpleName(simpleName: String) = modelClassFqNamesBySimpleName[simpleName]?.let { fqName ->
+        Class.forName(fqName)
+    } ?: throw IllegalArgumentException("Unknown model class: $simpleName")
 
-        members = membersByGetters.mapValues {
-            val memberShape = if (it.value.isPrimitive) {
-                it.value.name
-            } else if (it.value.name.startsWith("java.lang.")) {
-                it.value.simpleName
-            } else if (List::class.java.isAssignableFrom(it.value)) {
-                // TODO: lookup type parameter
-                it.key + "List" // TODO: save the shape
-            } else if (Set::class.java.isAssignableFrom(it.value)) {
-                // TODO: lookup type parameter
-                it.key + "Set" // TODO: save the shape
-            } else if (Map::class.java.isAssignableFrom(it.value)) {
-                // TODO: lookup type parameter
-                it.key + "Map" // TODO: save the shape
-            } else {
-                // TODO: save the shape
-                it.value.simpleName
+    private fun getOrCreateShape(clazz: Class<*>, typeParameterFqName: List<String> = emptyList(), shapeNameOverride: String? = null): Pair<String, Shape> =
+        (clazz.name + (if (typeParameterFqName.isEmpty()) "" else "<${typeParameterFqName.joinToString(", ")}>")).let { classFqName ->
+            var fillWithMembers = false
+
+            classFqNameToShape.getOrPut(clazz.canonicalName) {
+                val nameAndShape = if (clazz.isPrimitive) {
+                    clazz.name.firstLetterToUpperCase() to Shape().apply { type = clazz.name.toLowerCase() }
+                } else if (clazz.name.startsWith("java.lang.")) {
+                    clazz.simpleName to Shape().apply { type = clazz.simpleName.toLowerCase() }
+                } else if (clazz.name == "java.util.Date") {
+                    "Date" to Shape().apply { type = "timestamp" }
+                } else if (clazz.name.endsWith("[]") || clazz.name.namespace().let { it == "java.io" || it == "java.net" }) {
+                    throw UnmodelableOperation()
+                } else if (List::class.java.isAssignableFrom(clazz)) {
+                    if (typeParameterFqName.isEmpty()) throw IllegalStateException("Un-parameterized list")
+
+                    val listParameterFqName = typeParameterFqName.first()
+                    listParameterFqName.simpleName() + "List" to Shape().apply {
+                        type       = "list"
+                        listMember = Member().apply {
+                            shape = getOrCreateShape(Class.forName(listParameterFqName)).first
+                        }
+                    }
+                } else if (Map::class.java.isAssignableFrom(clazz)) {
+                    if (typeParameterFqName.isEmpty()) throw IllegalStateException("Un-parameterized map")
+
+                    val mapKeyParameterFqName   = typeParameterFqName[0]
+                    val mapValueParameterFqName = typeParameterFqName[1]
+                    mapKeyParameterFqName.simpleName() + "Map" to Shape().apply {
+                        type       = "map"
+                        mapKeyType = Member().apply {
+                            shape = getOrCreateShape(Class.forName(mapKeyParameterFqName)).first
+                        }
+                        mapValueType = Member().apply {
+                            shape = getOrCreateShape(Class.forName(mapValueParameterFqName)).first
+                        }
+                    }
+                } else {
+                    fillWithMembers = true
+                    clazz.simpleName to Shape().apply {
+                        type = "structure"
+                    }
+                }
+
+                // Override shape name
+                shapeNameOverride?.let { it to nameAndShape.second } ?: nameAndShape
+            }.apply {
+                // We fill shape members after shape is added to map to avoid infinite loops
+                if (fillWithMembers) {
+                    val membersByGetters = clazz.declaredMethods.filter { method ->
+                        method.name.startsWith("get") || method.name.startsWith("is")
+                    }.map { getter ->
+                        getter.name.mustNotStartWith("get").mustNotStartWith("is") to getter.returnType
+                    }.toMap()
+
+                    // TODO: implement
+                }
             }
-
-            Member().apply {
-                shape = memberShape
-            }
         }
-
-        type = "structure"
-
-        shapes[shapeName] = this@apply
-    }
 
     private fun createOperations(): List<Operation> {
         val requestMethodsToRequestClasses = serviceInterface.methods.groupBy { it.name }.mapValues {
@@ -100,41 +120,45 @@ class ModelFromAPIGenerator(
             "${it.key.name.firstLetterToUpperCase()}Request" == it.value.simpleName
         }
 
-        // TODO: implement
         return requestMethodsToRequestClasses.map {
-            val requestMethod = it.key
-            val requestClass  = it.value
-            val resultClass   = it.key.returnType
+            try {
+                val requestMethod = it.key
+                val requestClass  = it.value
+                val resultClass   = it.key.returnType
 
-            val operationName = requestMethod.name.firstLetterToUpperCase()
+                val operationName = requestMethod.name.firstLetterToUpperCase()
 
-            val httpMethod = verbToHttpMethod.keys.firstOrNull { operationName.startsWith(it) }?.let { verb ->
-                verbToHttpMethod[verb]
-            }?.name ?: throw IllegalStateException("Can't figure out HTTP method for $operationName")
+                val httpMethod = verbToHttpMethod.keys.firstOrNull { operationName.startsWith(it) }?.let { verb ->
+                    verbToHttpMethod[verb]
+                }?.name ?: throw IllegalStateException("Can't figure out HTTP method for $operationName")
 
-            val http = Http()
-                .withMethod(httpMethod)
-                .withRequestUri("/")
+                val http = Http()
+                    .withMethod(httpMethod)
+                    .withRequestUri("/")
 
-            val input = Input().apply {
-                shape = operationName + "Input"
-            }
-            createShape(input.shape, requestClass)
-
-            val output = Output().apply {
-                shape = if (resultClass.simpleName.endsWith("Result")) {
-                    operationName + "Output"
-                } else {
-                    resultClass.simpleName
+                val input = Input().apply {
+                    shape = operationName + "Input"
                 }
-            }
-            createShape(output.shape, resultClass)
+                getOrCreateShape(requestClass, shapeNameOverride = input.shape)
 
-            Operation()
-                .withName(operationName)
-                .withInput(input)
-                .withHttp(http).apply { setOutput(output) }
-        }
+                val output = Output().apply {
+                    shape = if (resultClass.simpleName.endsWith("Result")) {
+                        operationName + "Output"
+                    } else {
+                        resultClass.simpleName
+                    }
+                }
+                getOrCreateShape(resultClass, shapeNameOverride = output.shape)
+
+                Operation()
+                    .withName(operationName)
+                    .withInput(input)
+                    .withHttp(http).apply { setOutput(output) }
+            } catch (ume: UnmodelableOperation) {
+                // ignore this operation
+                null
+            }
+        }.filterNotNull()
     }
 
     fun generate() {
@@ -146,7 +170,7 @@ class ModelFromAPIGenerator(
             .serviceModel(ServiceModel(
                 serviceMetadata,
                 operations.associateBy { it.name },
-                shapes,
+                classFqNameToShape.values.toMap(),
                 HashMap<String, Authorizer>() // TODO: add authorizers
             )
         ).build()
@@ -155,3 +179,5 @@ class ModelFromAPIGenerator(
     }
 
 }
+
+class UnmodelableOperation : Exception()
