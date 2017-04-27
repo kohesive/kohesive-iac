26ab4870adcf203ddb36bfc53e33f7cb5ce160dd
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-04-27 13:24:52 +0700

    CloudTrail: CLI tool work in progress

--- model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudtrail/command/BaseCloudTrailCommand.kt
+++ model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudtrail/command/BaseCloudTrailCommand.kt
@@ -1,8 +1,13 @@
 package uy.kohesive.iac.model.aws.cloudtrail.command
 
+import com.amazonaws.services.cloudtrail.AWSCloudTrailClientBuilder
 import io.airlift.airline.Option
+import uy.kohesive.iac.model.aws.cloudtrail.CloudTrailAPIEventsProcessor
 import uy.kohesive.iac.model.aws.cloudtrail.EventsFilter
+import uy.kohesive.iac.model.aws.cloudtrail.EventsProcessor
+import uy.kohesive.iac.model.aws.cloudtrail.FileSystemEventsProcessor
 import uy.kohesive.iac.model.aws.cloudtrail.utils.DateTime
+import java.io.File
 
 abstract class BaseCloudTrailCommand {
 
@@ -17,6 +22,36 @@ abstract class BaseCloudTrailCommand {
 
     @Option(name = arrayOf("--endId"), description = "Event ID range end")
     val endId: String? = null
+
+    @Option(name = arrayOf("--local"), description = "Local filesystem mode")
+    var local = false
+
+    @Option(name = arrayOf("--events-dir"), description = "Events directory path")
+    val eventsDir: String? = null
+
+    protected fun createEventsProcessor(): EventsProcessor {
+        val eventsFilter = getFilter()
+
+        if (local) {
+            if (eventsDir == null) {
+                throw IllegalStateException("--events-dir parameter is expected to be non-empty for local mode")
+            }
+            return FileSystemEventsProcessor(
+                eventsDir       = File(eventsDir),
+                oneEventPerFile = false,
+                gzipped         = true,
+                eventsFilter    = eventsFilter
+           )
+        } else {
+            // TODO: allow configuring the client (profile, etc)
+            val awsClient = AWSCloudTrailClientBuilder.defaultClient()
+
+            return CloudTrailAPIEventsProcessor(
+                awsClient    = awsClient,
+                eventsFilter = eventsFilter
+            )
+        }
+    }
 
     protected fun getFilter() = EventsFilter(
         startId   = startId,
--- model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudtrail/command/ListEventsCommand.kt
+++ model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudtrail/command/ListEventsCommand.kt
@@ -1,12 +1,14 @@
 package uy.kohesive.iac.model.aws.cloudtrail.command
 
 import io.airlift.airline.Command
+import uy.kohesive.iac.model.aws.cloudtrail.CloudTrailEvent
 
 @Command(name = "list-events", description = "Lists CloudTrail events from CloudTrail API or local filesystem")
 class ListEventsCommand : BaseCloudTrailCommand(), Runnable {
 
     override fun run() {
-        // TODO: implement
+        println("AWS CloudTrailEvents matching the input filter ${getFilter()}\n")
+        createEventsProcessor().process(CloudTrailEvent::toString).forEach(::println)
     }
 
 }
