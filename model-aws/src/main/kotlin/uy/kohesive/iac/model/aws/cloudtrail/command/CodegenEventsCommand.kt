b90da9f3574a35496ac3f35d802536b535cd1e8f
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2017-04-27 17:03:19 +0700

    CloudTrail: codegen CLI command

--- model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudtrail/command/CodegenEventsCommand.kt
+++ model-aws/src/main/kotlin/uy/kohesive/iac/model/aws/cloudtrail/command/CodegenEventsCommand.kt
@@ -1,11 +1,53 @@
 package uy.kohesive.iac.model.aws.cloudtrail.command
 
 import io.airlift.airline.Command
+import io.airlift.airline.Option
+import uy.kohesive.iac.model.aws.cloudtrail.AWSApiCallBuilder
+import uy.kohesive.iac.model.aws.cloudtrail.AWSModelProvider
+import uy.kohesive.iac.model.aws.codegen.TemplateDescriptor
+import java.io.File
 
 @Command(name = "codegen", description = "Generates AWS API calls code from CloudTrail events fetched via API or stored on a local filesystem")
 class CodegenEventsCommand : BaseCloudTrailCommand(), Runnable {
 
+    @Option(name = arrayOf("--outputDir"), description = "Output base directory path", required = true)
+    lateinit var outputDir: String
+
+    @Option(name = arrayOf("--packageName"), description = "Generated classes package name", required = true)
+    lateinit var packageName: String
+
+    val awsModelProvider = AWSModelProvider()
+
     override fun run() {
-        // TODO: implement
+        var counter = 0
+
+        createEventsProcessor().process { event ->
+            if (listOf("Create", "Put", "Attach", "Run", "Set").any { event.eventName.startsWith(it) }) {
+                val serviceName = event.eventSource.split('.').first()
+
+                val awsModel = try {
+                    awsModelProvider.getModel(serviceName, event.apiVersion)
+                } catch (t: Throwable) {
+                    throw RuntimeException("Can't obtain an AWS model for $event", t)
+                }
+
+                val className    = "${event.eventName}Runner_${counter++}"
+                val classContent = AWSApiCallBuilder(
+                    intermediateModel = awsModel,
+                    event             = event,
+                    packageName       = packageName,
+                    className         = className
+                ).build(TemplateDescriptor.RequestRunner)
+
+                File(outputDir, "$className.kt").apply {
+                    writeText(classContent)
+                }
+            } else {
+                null
+            }
+        }.filterNotNull().forEach { outputFile ->
+            println("Done writing ${outputFile.path}")
+        }
     }
+
 }
